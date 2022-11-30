7e0c71d66fa45df3e5e0395d5f565a98df1ba2e8
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-30 10:27:37 +0530

    created an AudioInputStream using the AudioSystem class

--- src/main/java/io/github/sodhipinky/service/MusicPlayer.java
+++ src/main/java/io/github/sodhipinky/service/MusicPlayer.java
@@ -5,7 +5,11 @@
  */
 package io.github.sodhipinky.service;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
+import java.io.IOException;
 
 public class MusicPlayer {
     public void play() {
@@ -13,5 +17,11 @@ public class MusicPlayer {
         String songPath = "src/main/resources/Wakanda Origins.wav";
         //2. create a File object to represent the song file
         File songFile = new File(songPath);
+        //3. create an object of AudioInputStream class
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(songFile);
+        } catch (UnsupportedAudioFileException | IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
